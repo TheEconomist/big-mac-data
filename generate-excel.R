ed4e66be346d7e3a9d97fc5c31f08a48311aa406
Evan Hensleigh <futuraprime@gmail.com>
2018-07-06 15:28:39 +0100

    Fixing rounding on Excel data.

--- generate-excel.R
+++ generate-excel.R
@@ -13,16 +13,16 @@ data = fread('./output-data/big-mac-full-index.csv') %>%
         dollar_price,
         dollar_ppp = dollar_ex * dollar_price / .SD[currency_code == 'USD']$dollar_price,
         GDP_dollar,
-        dollar_valuation = USD_raw,
-        euro_valuation = EUR_raw,
-        sterling_valuation = GBP_raw,
-        yen_valuation = JPY_raw,
-        yuan_valuation = CNY_raw,
-        dollar_adj_valuation = USD_adjusted,
-        euro_adj_valuation = EUR_adjusted,
-        sterling_adj_valuation = GBP_adjusted,
-        yen_adj_valuation = JPY_adjusted,
-        yuan_adj_valuation = CNY_adjusted
+        dollar_valuation = USD_raw * 100,
+        euro_valuation = EUR_raw * 100,
+        sterling_valuation = GBP_raw * 100,
+        yen_valuation = JPY_raw * 100,
+        yuan_valuation = CNY_raw * 100,
+        dollar_adj_valuation = USD_adjusted * 100,
+        euro_adj_valuation = EUR_adjusted * 100,
+        sterling_adj_valuation = GBP_adjusted * 100,
+        yen_adj_valuation = JPY_adjusted * 100,
+        yuan_adj_valuation = CNY_adjusted * 100
     ), by=date]
 
 dates = data$date %>% unique
@@ -35,4 +35,4 @@ for(sheetDate in sort(dates, decreasing = T)) {
     xlsx.addTable(wb, sheet, data[date == sheetDate, -1], row.names=FALSE, startCol=1)
 }
 
-saveWorkbook(wb, paste0('big-mac-',max(dates),'.xls'))
+saveWorkbook(wb, paste0('./output-data/big-mac-',max(dates),'.xls'))
